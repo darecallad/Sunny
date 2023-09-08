e6415ca480017fed1018a5ce66885b2f2966fadd
Johnson <Ashane199080706@hotmail.com.tw>
2023-09-08 15:44:48 -0700

    update testimonals

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,3 +1,30 @@
+document
+  .getElementById("prevTestimonial")
+  .addEventListener("click", function () {
+    navigateTestimonial(-1);
+  });
+
+document
+  .getElementById("nextTestimonial")
+  .addEventListener("click", function () {
+    navigateTestimonial(1);
+  });
+
+function navigateTestimonial(direction) {
+  const testimonials = document.querySelectorAll(".testimonial");
+  let activeIndex = 0;
+  testimonials.forEach((testimonial, index) => {
+    if (testimonial.classList.contains("active")) {
+      activeIndex = index;
+    }
+  });
+
+  const newIndex =
+    (activeIndex + direction + testimonials.length) % testimonials.length;
+  testimonials[activeIndex].classList.remove("active");
+  testimonials[newIndex].classList.add("active");
+}
+
 const collapsibles = document.querySelectorAll(".collapsible");
 collapsibles.forEach((item) =>
   item.addEventListener("click", function () {
