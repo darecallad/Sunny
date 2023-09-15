16f981206804276e1d1e6c01b9905e4042fb9048
Johnson <Ashane199080706@hotmail.com.tw>
2023-09-15 16:02:06 -0700

    update waitng list

---
+++ docs/js/emailrequest.js
@@ -0,0 +1,25 @@
+document.getElementById("emailForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const message = document.getElementById("message").value;
+
+  fetch("/send-email", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      name: name,
+      email: email,
+      message: message,
+    }),
+  }).then((response) => {
+    if (response.ok) {
+      alert("Email sent!");
+    } else {
+      alert("Failed to send email.");
+    }
+  });
+});
