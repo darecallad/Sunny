18b6905fb91d489799b87e9f5f337ade0e9b93fd
Johnson <Ashane199080706@hotmail.com.tw>
2023-09-15 17:24:52 -0700

    update backend

---
+++ server/index.js
@@ -0,0 +1,37 @@
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const nodemailer = require("nodemailer");
+const app = express();
+
+app.use(bodyParser.json());
+
+app.post("/send-email", (req, res) => {
+  let transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+  });
+
+  let mailOptions = {
+    from: req.body.email,
+    to: "targetEmail@gmail.com",
+    subject: `Message from ${req.body.name}`,
+    text: req.body.message,
+  };
+
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+      res.status(500).send("Error while sending email.");
+    } else {
+      res.send("Email sent.");
+    }
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Server started on http://localhost:3000");
+});
