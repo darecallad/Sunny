6cd9ed77979ccbd97b2dbca9f1daf862513bf5f7
Johnson <Ashane199080706@hotmail.com.tw>
2023-09-26 21:22:13 -0700

    update logic for nav bar

--- docs/js/collapsibles.js
+++ docs/js/collapsibles.js
@@ -4,3 +4,58 @@ collapsibles.forEach((item) =>
     this.classList.toggle("collapsible--expanded");
   })
 );
+
+function mobileNavBehavior() {
+  const navClickSpans = document.querySelectorAll(".nav__click > span");
+  navClickSpans.forEach((span) =>
+    span.addEventListener("click", function (e) {
+      const parentItem = span.parentElement;
+      const subnav = parentItem.querySelector(".subnav__content");
+
+      if (subnav.style.visibility === "hidden" || !subnav.style.visibility) {
+        subnav.style.visibility = "visible";
+        subnav.style.opacity = "1";
+        subnav.style.maxHeight = "23rem";
+        subnav.style.transform = "translateY(0)";
+      } else {
+        subnav.style.visibility = "hidden";
+        subnav.style.opacity = "0";
+        subnav.style.maxHeight = "0";
+        subnav.style.transform = "translateY(-100%)";
+      }
+
+      e.stopPropagation();
+    })
+  );
+}
+
+function desktopNavBehavior() {
+  // 你可以在這裡添加專為桌面版的功能或樣式
+  // ...
+}
+
+document.addEventListener("click", function (e) {
+  const subnavs = document.querySelectorAll(".subnav__content");
+  subnavs.forEach((subnav) => {
+    if (!e.target.closest(".nav__click")) {
+      subnav.style.visibility = "hidden";
+      subnav.style.opacity = "0";
+      subnav.style.maxHeight = "0";
+      subnav.style.transform = "translateY(-100%)";
+    }
+  });
+});
+
+function adjustLayout() {
+  if (window.innerWidth > 768) {
+    desktopNavBehavior();
+  } else {
+    mobileNavBehavior();
+  }
+}
+
+// 當文檔加載時執行
+adjustLayout();
+
+// 當視窗大小改變時執行
+window.addEventListener("resize", adjustLayout);
--- docs/js/main.js
+++ docs/js/main.js
@@ -43,7 +43,79 @@ document.querySelector(".read-more").addEventListener("click", function () {
     this.textContent = "Read Less";
   }
 });
+function mobileNavBehavior() {
+  const navClickSpans = document.querySelectorAll(".nav__click > span");
+  navClickSpans.forEach((span) =>
+    span.addEventListener("click", function (e) {
+      const parentItem = span.parentElement;
+      const subnav = parentItem.querySelector(".subnav__content");
 
+      if (subnav.style.visibility === "hidden" || !subnav.style.visibility) {
+        subnav.style.visibility = "visible";
+        subnav.style.opacity = "1";
+        subnav.style.maxHeight = "23rem";
+        subnav.style.transform = "translateY(0)";
+      } else {
+        subnav.style.visibility = "hidden";
+        subnav.style.opacity = "0";
+        subnav.style.maxHeight = "0";
+        subnav.style.transform = "translateY(-100%)";
+      }
+
+      e.stopPropagation();
+    })
+  );
+}
+
+function desktopNavBehavior() {
+  const navClickSpans = document.querySelectorAll(".nav__click > span");
+  navClickSpans.forEach((span) =>
+    span.addEventListener("click", function (e) {
+      const parentItem = span.parentElement;
+      const subnav = parentItem.querySelector(".subnav__content");
+
+      if (subnav.style.visibility === "hidden" || !subnav.style.visibility) {
+        subnav.style.visibility = "visible";
+        subnav.style.opacity = "1";
+        subnav.style.maxHeight = "23rem";
+        subnav.style.transform = "translateY(0)";
+      } else {
+        subnav.style.visibility = "hidden";
+        subnav.style.opacity = "0";
+        subnav.style.maxHeight = "0";
+        subnav.style.transform = "translateY(-100%)";
+      }
+
+      e.stopPropagation();
+    })
+  );
+}
+
+document.addEventListener("click", function (e) {
+  const subnavs = document.querySelectorAll(".subnav__content");
+  subnavs.forEach((subnav) => {
+    if (!e.target.closest(".nav__click")) {
+      subnav.style.visibility = "hidden";
+      subnav.style.opacity = "0";
+      subnav.style.maxHeight = "0";
+      subnav.style.transform = "translateY(-100%)";
+    }
+  });
+});
+
+function adjustLayout() {
+  if (window.innerWidth > 768) {
+    desktopNavBehavior();
+  } else {
+    mobileNavBehavior();
+  }
+}
+
+// 當文檔加載時執行
+adjustLayout();
+
+// 當視窗大小改變時執行
+window.addEventListener("resize", adjustLayout);
 document.addEventListener("DOMContentLoaded", function () {
   let currentIndex = 0;
   const pictures = document.querySelectorAll(".picture-container picture");
