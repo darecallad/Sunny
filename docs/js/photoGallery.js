9a8e5aec002420f48e30ed244802658f980208e4
Johnson <Ashane199080706@hotmail.com.tw>
2023-09-26 18:09:11 -0700

    update photo for center

---
+++ docs/js/photoGallery.js
@@ -0,0 +1,85 @@
+var currentPhotoIndex = 0;
+var totalPhotos;
+var photoThumbnails = document.querySelectorAll(".photo-thumbnail");
+
+function setTotalPhotosBasedOnViewport() {
+  var width = window.innerWidth;
+
+  if (width <= 480) {
+    totalPhotos = 2;
+  } else if (width <= 768) {
+    totalPhotos = 4;
+  } else {
+    totalPhotos = 6;
+  }
+
+  showPhotos();
+}
+
+function showPhotos() {
+  for (var i = 0; i < photoThumbnails.length; i++) {
+    if (i >= currentPhotoIndex && i < currentPhotoIndex + totalPhotos) {
+      photoThumbnails[i].style.display = "block";
+    } else {
+      photoThumbnails[i].style.display = "none";
+    }
+  }
+}
+
+function openModalWithPhoto(src) {
+  document.getElementById("modal-content").src = src;
+  var modal = document.getElementById("photo-modal");
+  modal.style.display = "flex";
+}
+
+function closePhotoModal() {
+  document.getElementById("photo-modal").style.display = "none";
+}
+
+window.onclick = function (event) {
+  var modal = document.getElementById("photo-modal");
+  if (event.target === modal) {
+    closePhotoModal();
+  }
+};
+
+function showPreviousPhotos() {
+  if (currentPhotoIndex > 0) {
+    currentPhotoIndex--;
+    showPhotos();
+  }
+}
+
+function showNextPhotos() {
+  if (currentPhotoIndex < photoThumbnails.length - totalPhotos) {
+    currentPhotoIndex++;
+    showPhotos();
+  }
+}
+
+function showPreviousModalPhoto() {
+  if (currentPhotoIndex > 0) {
+    currentPhotoIndex--;
+    let newSrc = photoThumbnails[currentPhotoIndex].querySelector("img").src;
+    document.getElementById("modal-content").src = newSrc;
+  }
+}
+
+function showNextModalPhoto() {
+  if (currentPhotoIndex < photoThumbnails.length - 1) {
+    currentPhotoIndex++;
+    let newSrc = photoThumbnails[currentPhotoIndex].querySelector("img").src;
+    document.getElementById("modal-content").src = newSrc;
+  }
+}
+
+setTotalPhotosBasedOnViewport();
+window.addEventListener("resize", setTotalPhotosBasedOnViewport);
+showPhotos(); // 初始化页面显示
+
+document
+  .querySelector(".left-arrow")
+  .addEventListener("click", showPreviousPhotos);
+document
+  .querySelector(".right-arrow")
+  .addEventListener("click", showNextPhotos);
