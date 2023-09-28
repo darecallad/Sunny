ff6635e1577d196fb574c484d60718f91b5ae6f9
Johnson <Ashane199080706@hotmail.com.tw>
2023-09-28 11:30:01 -0700

    fixed navbar problem

--- docs/js/collapsibles.js
+++ docs/js/collapsibles.js
@@ -5,31 +5,7 @@ collapsibles.forEach((item) =>
   })
 );
 
-function mobileNavBehavior() {
-  const navClickSpans = document.querySelectorAll(".nav__click > span");
-  navClickSpans.forEach((span) =>
-    span.addEventListener("click", function (e) {
-      const parentItem = span.parentElement;
-      const subnav = parentItem.querySelector(".subnav__content");
-
-      if (subnav.style.visibility === "hidden" || !subnav.style.visibility) {
-        subnav.style.visibility = "visible";
-        subnav.style.opacity = "1";
-        subnav.style.maxHeight = "23rem";
-        subnav.style.transform = "translateY(0)";
-      } else {
-        subnav.style.visibility = "hidden";
-        subnav.style.opacity = "0";
-        subnav.style.maxHeight = "0";
-        subnav.style.transform = "translateY(-100%)";
-      }
-
-      e.stopPropagation();
-    })
-  );
-}
-
-function desktopNavBehavior() {
+function navBehavior() {
   const navClickSpans = document.querySelectorAll(".nav__click > span");
   navClickSpans.forEach((span) =>
     span.addEventListener("click", function (e) {
@@ -65,15 +41,7 @@ document.addEventListener("click", function (e) {
   });
 });
 
-function adjustLayout() {
-  if (window.innerWidth > 768) {
-    desktopNavBehavior();
-  } else {
-    mobileNavBehavior();
-  }
-}
-// 當文檔加載時執行
-adjustLayout();
+navBehavior();
 
 // 當視窗大小改變時執行
 window.addEventListener("resize", adjustLayout);
--- docs/js/main.js
+++ docs/js/main.js
@@ -43,7 +43,7 @@ document.querySelector(".read-more").addEventListener("click", function () {
     this.textContent = "Read Less";
   }
 });
-function mobileNavBehavior() {
+function navBehavior() {
   const navClickSpans = document.querySelectorAll(".nav__click > span");
   navClickSpans.forEach((span) =>
     span.addEventListener("click", function (e) {
@@ -54,30 +54,6 @@ function mobileNavBehavior() {
         subnav.style.visibility = "visible";
         subnav.style.opacity = "1";
         subnav.style.maxHeight = "23rem";
-        subnav.style.transform = "translateY(0)";
-      } else {
-        subnav.style.visibility = "hidden";
-        subnav.style.opacity = "0";
-        subnav.style.maxHeight = "0";
-        subnav.style.transform = "translateY(-100%)";
-      }
-
-      e.stopPropagation();
-    })
-  );
-}
-
-function desktopNavBehavior() {
-  const navClickSpans = document.querySelectorAll(".nav__click > span");
-  navClickSpans.forEach((span) =>
-    span.addEventListener("click", function (e) {
-      const parentItem = span.parentElement;
-      const subnav = parentItem.querySelector(".subnav__content");
-
-      if (subnav.style.visibility === "hidden" || !subnav.style.visibility) {
-        subnav.style.visibility = "visible";
-        subnav.style.opacity = "1";
-        subnav.style.maxHeight = "auto";
         subnav.style.transform = "translateY(0)";
       } else {
         subnav.style.visibility = "hidden";
@@ -103,19 +79,8 @@ document.addEventListener("click", function (e) {
   });
 });
 
-function adjustLayout() {
-  if (window.innerWidth > 768) {
-    desktopNavBehavior();
-  } else {
-    mobileNavBehavior();
-  }
-}
+navBehavior();
 
-// 當文檔加載時執行
-adjustLayout();
-
-// 當視窗大小改變時執行
-window.addEventListener("resize", adjustLayout);
 document.addEventListener("DOMContentLoaded", function () {
   let currentIndex = 0;
   const pictures = document.querySelectorAll(".picture-container picture");
