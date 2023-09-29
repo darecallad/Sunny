7da3f4df4310cfa5808e4a98b97513851ddbf2d7
Johnson <Ashane199080706@hotmail.com.tw>
2023-09-29 12:36:11 -0700

    update

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const cors = require("cors");
 app.use(bodyParser.json());
 
 const corsOptions = {
-  origin: ["https://darecallad.github.io", "https://www.sunnychildcare.com"],
+  origin: "https://www.sunnychildcare.com",
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   preflightContinue: false,
   optionsSuccessStatus: 204,
