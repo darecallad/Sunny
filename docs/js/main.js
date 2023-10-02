4395aca8bcd0ddc285a5938db941e53ad2355931
Johnson <Ashane199080706@hotmail.com.tw>
2023-10-02 09:55:20 -0700

    tiny update

--- docs/js/main.js
+++ docs/js/main.js
@@ -125,19 +125,3 @@ document.addEventListener("click", function (e) {
 });
 
 navBehavior();
-
-document.addEventListener("DOMContentLoaded", function () {
-  let currentIndex = 0;
-  const pictures = document.querySelectorAll(".picture-container picture");
-
-  setInterval(function () {
-    pictures[currentIndex].style.display = "none";
-
-    currentIndex++;
-    if (currentIndex >= pictures.length) {
-      currentIndex = 0;
-    }
-
-    pictures[currentIndex].style.display = "block";
-  }, 3000);
-});
