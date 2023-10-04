6af3656d921b8cf050b9f64ad60d9d7dc7abe648
Johnson <Ashane199080706@hotmail.com.tw>
2023-10-04 14:37:40 -0700

    update map

--- docs/js/map.js
+++ docs/js/map.js
@@ -1,7 +1,12 @@
 function initMap() {
-  var center = { lat: -34.397, lng: 150.644 }; //這裡的經緯度是示範用，您可以更改成您想顯示的位置
+  var center = { lat: 37.379112243652344, lng: -121.93821716308594 };
   var map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 8,
+    zoom: 11,
     center: center,
+  });
+  var marker = new google.maps.Marker({
+    position: center,
+    map: map,
+    title: "Sunny Child Care",
   });
 }
