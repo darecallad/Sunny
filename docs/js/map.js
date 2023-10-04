e9f160276cb1fe7fbde270a9775927d463834af5
Johnson <Ashane199080706@hotmail.com.tw>
2023-10-04 15:42:22 -0700

    update map size

--- docs/js/map.js
+++ docs/js/map.js
@@ -1,7 +1,7 @@
 function initMap() {
   var center = { lat: 37.379112243652344, lng: -121.93821716308594 };
   var map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 11,
+    zoom: 12,
     center: center,
   });
   var marker = new google.maps.Marker({
